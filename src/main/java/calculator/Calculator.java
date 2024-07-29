c20116da2fe87f7c654f1ce3d06302a436af71d5
BoSung308 <qhtjd2719@naver.com>
2024-07-30 01:13:36 +0900

    Level2 - 6(계산기 요구사항) 생성자 초기화

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,7 +2,7 @@ package calculator;
 
 import java.util.ArrayList;
 public class Calculator {
-    private final ArrayList<Integer> list = new ArrayList<>();
+    private final ArrayList<Integer> list;
     private ArrayList<Integer> inquiryResults;
     private int results;
     private int index;
@@ -11,9 +11,10 @@ public class Calculator {
     // 현재 list는 ArrayList 객체를 참조하지않는상태이고 객체를 생성하여 참조할수 있는 준비 상태
     // 왜 ArrayList 컬렉션을 사용했을까?
 
-   /*     public Calculator() {
-
-    }*/
+    public Calculator() {
+        this.list = new ArrayList<>();
+        // 생성자로 인한 초기화
+    }
     // 'Calculator' 클래스의 생성자 정의,, 생성자는 객체가 생성될 떄 자동으로 호출되는 메서드
     // 클래스 내에서 Integer 타입의 객체를 저장할 수 있는 ArrayList 참조
     // 생성자를 정의함으로써 keep 필드에 생성된 ArrayList 객체 할당, keep 필드는 빈 ArrayList 참조
@@ -70,22 +71,3 @@ public class Calculator {
         return result;
     }
 }
-
-//        int result = 0;
-//        if (operator == '+') {
-//            result = num1 + num2;
-//        } else if (operator == '-') {
-//            result = num1 - num2;
-//        } else if (operator == '*') {
-//            result = num1 * num2;
-//        } else if (operator == '/') {
-//            try {
-//                if (num2 == 0) {
-//                    throw new ArithmeticException ("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-//                }
-//                result = num1 / num2;
-//            } catch (ArithmeticException  e) {
-//                e.printStackTrace();
-//                System.out.println("올바른 사칙연산을 입력하세요");
-// 예외가 발생한 지점과 메서드 호출 경로를 출력 -> 이를 통해 예외가 어디서 발생했는지, 어떻게 전파되었는지를 알 수 있음.
-//}

9937935b8c8a6ac0c0b93c00464dc98c0bf4242c
BoSung308 <qhtjd2719@naver.com>
2024-07-30 21:36:18 +0900

    Level2 - 7(계산기 요구사항)

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,102 +1,80 @@
 package calculator;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 // import java.util.LinkedList;
 public class App {
     public static void main(String[] args) {
-        //LinkedList<Integer> intlist = new LinkedList<>();
-         int [] numArrays = new int[10];
-        // 10개의 배열을 선언한다는 의미에서 numArrays
-        Calculator cal = new Calculator();
-        ArrayList<Integer> results = new ArrayList<>();
-
-        // Calculator 클래스로부터 인스턴스 생성
-
-        int index = 0;
-        // result 값이 저장된 공간의 수
         Scanner sc = new Scanner(System.in);
+        Calculator cal = new Calculator();
+        // Calculator 클래스로부터 인스턴스 생성
+        int conditionNum;
 
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요 : ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요 : ");
-            int num2 = sc.nextInt();
+            System.out.println("사칙연산을 진행하시겠습니까? or 원의 넓이를 구하시겠습니까?");
+            System.out.println("사칙연산을 원하신다면 '1'을 원의 넓이를 구하고싶다면 '2'를 입력해주세요");
 
-            System.out.println("사칙연산 기호를 입력하세요 : ");
-            char operator = sc.next().charAt(0);
-            // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자하는 문자열의 위치를 받음.
-            int result = cal.calculate(num1, num2, operator);
+            conditionNum = sc.nextInt();
+            if (conditionNum == 1) {
+                System.out.print("첫 번째 숫자를 입력하세요 : ");
+                int num1 = sc.nextInt();
+                // 사용자로부터 입력된 값을 정수형으로 반환하여 num1에 저장
+                System.out.print("두 번째 숫자를 입력하세요 : ");
+                int num2 = sc.nextInt();
+
+                System.out.println("사칙연산 기호를 입력하세요 : ");
+                char operator = sc.next().charAt(0);
+                // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자하는 문자열의 위치를 받음.
+                int result = cal.calculate(num1, num2, operator);
+           /* if (Double.isNaN(result)) {
+                continue; // 오류가 발생, 루프 시작으로 초기화
+                // nan = not-a-number, 숫자가 아닌 결과,,ex) 0을 0으로 나누는 경우 nan
+            }*/
+                System.out.println("계산 결과 : " + result);
+
+                cal.setList(result);
+                System.out.println("현재 저장된 결과 : " + cal.getList());
+
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.next();
+                if (remove.equals("remove")) {
+                    cal.removeResult(remove);
+                }
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = sc.next();
+                if (inquiry.equals("inquiry")) {
+                    cal.inquiryResults(result);
+                }
 
 
-              // nan = not-a-number, 숫자가 아닌 결과,,ex) 0을 0으로 나누는 경우 nan
-            System.out.println("계산 결과 : " + result);
-
-            cal.addResult(result);
-            //
-            System.out.println("현재 저장된 결과 : " + cal.getList());
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String remove = sc.next();
-            if (remove.equals("remove")) {
-               cal.removeResult(remove);
+            } else if (conditionNum == 2) {
+                System.out.println("반지름을 입력하세요.(단, 정수가 아닌 실수(ex : 2.0) 형태로 입력해야 합니다.)");
+                double inputRadius = sc.nextDouble();
+                double circleResult = cal.calculateCircleArea(inputRadius);
+                System.out.println("원의 넓이 : " + circleResult);
+                 System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = sc.next();
+                if (inquiry.equals("inquiry")) {
+                    cal.circleResultInquiry(circleResult);
+                }
             }
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiry = sc.next();
-            if(inquiry.equals("inquiry")){
-                cal.inquiryResults(result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String finish;
+            try {
+                finish = sc.next();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-
-
-//            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-//            String inquiry = sc.next();
-//            if (inquiry.equals("inquiry")) {
-//                for (int intlist_inquiry : intlist) {
-//                    System.out.print("[");
-//                    System.out.print(intlist_inquiry);
-//                    System.out.print("]");
-//                }
-//                System.out.println();
-//            }
-//                if (nextIndex < numArrays.length) {
-//                // 배열이 다 차지 않은 상태            }
-//                numArrays[nextIndex] = result;
-//                // 결과 값을 배열의  인덱스에 저장
-//                nextIndex++; // 문...
-//
-//
-//                for (int i = 1; i < numArrays.length; i++) {
-//                    //배열이 꽉 찼을 때
-//                    numArrays[i - 1] = numArrays[i];
-//                    // 새로운 값을 배열 마지막에 넣음
-//
-//            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-//            String finish = null;
-//            try {
-//                finish = sc.next();
-//            } catch (Exception e) {
-//                throw new RuntimeException(e);
-//            }
-//            if (finish.equals("exit")) {
-//                break;
-//            }
-//            System.out.println("숫자를 더 입력받겠습니다.");
-//            // else문에서 sout사용할때 가독성이 안좋기 때문에 if문 닫고 sout으로 가능한 해결하는 것이 좋음
+            if (finish.equals("exit")) {
+                break;
+            }
+            System.out.println("숫자를 더 입력받겠습니다.");
+            // else문에서 sout사용할때 가독성이 안좋기 때문에 if문 닫고 sout으로 가능한 해결하는 것이 좋음
         }
     }
 }
 
-
-
-            // intlist 인덱스0의 저장값을 삭제
-
-
-
-//            int results = 0;
-//            cal.setKeep(results);
-//            System.out.println("모든 계산 결과 : " + cal.getKeep());
-
-//        if (Double.isNaN(result)) {
-//            continue; // 오류가 발생, 루프 시작으로 초기화
-//        }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,15 @@
 package calculator;
 
 import java.util.ArrayList;
+
 public class Calculator {
-    private final ArrayList<Integer> list;
-    private ArrayList<Integer> inquiryResults;
-    private int results;
-    private int index;
+    protected final ArrayList<Integer> list;
+    protected final ArrayList<Double> results;
+    // list를 final로 선언하며 list가 다른 'ArrayList'객체를 가리키도록 변경 불가하게 final 사용
+    // 하지만 list에 요소를 추가 또는 제거는 가능,, 참조가 불변인것이지 객체 자체가 불변은 아님
+    protected ArrayList<Integer> circleResults;
+    final double PI = Math.PI;
+    // 파이값은 절대적인 값이기때문에 final을 이용하여 불변으로 정해놈
 
     // ArrayList<Integer> 타입의 필드 선언, list는 Integer 객체를 저장하는 리스트 참조
     // 현재 list는 ArrayList 객체를 참조하지않는상태이고 객체를 생성하여 참조할수 있는 준비 상태
@@ -13,27 +17,53 @@ public class Calculator {
 
     public Calculator() {
         this.list = new ArrayList<>();
+        this.circleResults = new ArrayList<>();
+        this.results = new ArrayList<>();
         // 생성자로 인한 초기화
     }
+
     // 'Calculator' 클래스의 생성자 정의,, 생성자는 객체가 생성될 떄 자동으로 호출되는 메서드
     // 클래스 내에서 Integer 타입의 객체를 저장할 수 있는 ArrayList 참조
     // 생성자를 정의함으로써 keep 필드에 생성된 ArrayList 객체 할당, keep 필드는 빈 ArrayList 참조
     public ArrayList<Integer> getList() {
         return list;
     }
-    public void addResult(int result) {
+
+    public void setList(int result) {
         list.add(result);
     }
 
-    public void removeResult(String remove){
+    public void removeResult(String remove) {
         list.remove(0);
         System.out.println("제일 먼저 저장된 결과" + remove + "가 삭제되었습니다.");
     }
 
-
-    public void inquiryResults(int result){
-        for(int i = 0; i < list.size(); i++){
+    public void inquiryResults(int result) {
+        for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+
+    public double calculateCircleArea(double number) {
+        double radius = number;
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+
+    public ArrayList<Double> getRadius() {
+        return results;
+    }
+
+    public void setRadius(Double circleResult) {
+        results.add(circleResult);
+    }
+
+    public void circleResultInquiry(double circleResult) {
+        for(double res : results){
+            System.out.print(res);
+            System.out.print(" ");
         }
         System.out.println();
     }
@@ -41,7 +71,7 @@ public class Calculator {
 
     public int calculate(int num1, int num2, char operator) {
         // 객체가 가지고 있어야할 메서드 정의
-        int result =0;
+        int result = 0;
         switch (operator) {
             case '+': {
                 result = num1 + num2;
